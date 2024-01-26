995ec3ffd3c6381f705732fd07056b9f9712958c
markymelnik <markymelnik@gmail.com>
2024-01-26 12:47:18 -0500

    configure server settings

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,12 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-import App from './App.js';
+import App from './App.tsx';
 
 describe('App Component', () => {
   test('renders Logo component', () => {
     render(<App />);
     const logoElement = screen.getByText(/NBA Battle/i);
-    expect(logoElement).toBeInTheDocument();
+    expect(logoElement).toBeTruthy();
   });
 });
