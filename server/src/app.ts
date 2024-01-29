87133235a4a3bdf0d73cdf2391a78ea46899e65a
markymelnik <markymelnik@gmail.com>
2024-01-29 14:13:06 -0500

    configure cors and env variables

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
 import { signupUser } from './api/controllers/signupUser';
 import { loginUser } from './api/controllers/loginUser';
 import { validateToken } from './api/middleware/validateToken';
 
+const FRONTEND_URL = process.env.FRONTEND_URL;
+const FRONTEND_PORT = process.env.FRONTEND_PORT;
+const BACKEND_PORT = process.env.BACKEND_PORT;
+
 const app = express();
+
 app.use(express.json());
+app.use(cors({ origin: `${FRONTEND_URL}:${FRONTEND_PORT}`}))
 
 app.get('/', (req, res) => {
   res.send('NBA Battle');
@@ -18,9 +26,8 @@ app.post('/signup', signupUser);
 
 app.post('/login', loginUser);
 
-const port = 4000;
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+app.listen(`${BACKEND_PORT}`, () => {
+  console.log(`Server listening at ${BACKEND_PORT}`);
 });
 
 export default app;
