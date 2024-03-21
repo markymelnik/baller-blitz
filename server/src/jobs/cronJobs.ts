558c267ec2e845d780dd87a547f6e71ffd754a24
markymelnik <markymelnik@gmail.com>
2024-03-21 11:01:29 -0400

    configure cron jobs to use Eastern Time

--- server/src/jobs/cronJobs.ts
+++ server/src/jobs/cronJobs.ts
@@ -4,15 +4,22 @@ import { PredictionController } from '../api/PredictionController';
 
 export function initCron() {
   const cronTimezone = 'America/New_York';
-  
-  cron.schedule('0 15 * * *', () => {
-    console.log('cron job run')
-  }, {
+
+  cron.schedule('0,15,30,45 20-23 * * *', GameController.updateGamesInDatabase, {
     timezone: cronTimezone,
   });
 
-  cron.schedule('0,15,30,45 20-23 * * *', GameController.updateGamesInDatabase);
-  cron.schedule('0,15,30 0-1 * * *', GameController.updateGamesInDatabase);
-  cron.schedule('15,30 1 * * *', PredictionController.updatePredictionsInDatabase);
-  cron.schedule('5 12 * * *', GameController.fetchAndStoreGamesToday);
+  cron.schedule('0,15,30 0-1 * * *', GameController.updateGamesInDatabase, {
+    timezone: cronTimezone,
+  });
+
+  cron.schedule('15,30 1 * * *', PredictionController.updatePredictionsInDatabase, {
+    timezone: cronTimezone,
+  });
+
+  cron.schedule('5 12 * * *', GameController.fetchAndStoreGamesToday, {
+    timezone: cronTimezone,
+  });
 }
+
+//
